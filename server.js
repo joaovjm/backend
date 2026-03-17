ce61e260cee417edb4242ef2259f6cf7a92bbb72
joaovjm <joao.oliveira18.jm@gmail.com>
2026-03-17 01:44:20 -0300

    Refactor server configuration: replace conditional dotenv import with direct configuration call for improved clarity and consistency.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
+import dotenv from "dotenv";
 import app from "./app.js";
 
-if(process.env.NODE_ENV !== "production"){
-  await import ("dotenv/config")
-}
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
